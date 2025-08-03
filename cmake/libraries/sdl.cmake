2a78824b309737e1326ab4b7bbcc174f527ab2ca
Tim Angus <tim@ngus.net>
2025-08-03 22:18:19 +0100

    Update to SDL3

--- cmake/libraries/sdl.cmake
+++ cmake/libraries/sdl.cmake
@@ -2,7 +2,7 @@ if(NOT BUILD_CLIENT)
     return()
 endif()
 
-set(INTERNAL_SDL_DIR ${SOURCE_DIR}/thirdparty/SDL2-2.32.8)
+set(INTERNAL_SDL_DIR ${SOURCE_DIR}/thirdparty/SDL3-3.2.18)
 
 include(utils/arch)
 
@@ -12,7 +12,7 @@ if(WIN32 OR APPLE)
 endif()
 
 if(USE_INTERNAL_SDL AND HAVE_INTERNAL_SDL)
-    set(SDL2_INCLUDE_DIRS ${INTERNAL_SDL_DIR}/include)
+    set(SDL3_INCLUDE_DIRS ${INTERNAL_SDL_DIR}/include)
     list(APPEND CLIENT_DEFINITIONS USE_INTERNAL_SDL_HEADERS)
     list(APPEND RENDERER_DEFINITIONS USE_INTERNAL_SDL_HEADERS)
 
@@ -26,32 +26,29 @@ if(USE_INTERNAL_SDL AND HAVE_INTERNAL_SDL)
         endif()
 
         if(MINGW)
-            set(SDL2_LIBRARIES
-                ${LIB_DIR}/libSDL2main.a
-                ${LIB_DIR}/libSDL2.dll.a)
+            set(SDL3_LIBRARIES
+                ${LIB_DIR}/libSDL3.dll.a)
         elseif(MSVC)
-            set(SDL2_LIBRARIES
-                ${LIB_DIR}/SDL2main.lib
-                ${LIB_DIR}/SDL2.lib)
+            set(SDL3_LIBRARIES
+                ${LIB_DIR}/SDL3.lib)
         endif()
 
-        list(APPEND CLIENT_DEPLOY_LIBRARIES ${LIB_DIR}/SDL2.dll)
+        list(APPEND CLIENT_DEPLOY_LIBRARIES ${LIB_DIR}/SDL3.dll)
     elseif(APPLE)
-        set(SDL2_LIBRARIES
-            ${SOURCE_DIR}/thirdparty/libs/macos/libSDL2main.a
-            ${SOURCE_DIR}/thirdparty/libs/macos/libSDL2-2.0.0.dylib)
+        set(SDL3_LIBRARIES
+            ${SOURCE_DIR}/thirdparty/libs/macos/libSDL3.dylib)
         list(APPEND CLIENT_DEPLOY_LIBRARIES
-            ${SOURCE_DIR}/thirdparty/libs/macos/libSDL2-2.0.0.dylib)
+            ${SOURCE_DIR}/thirdparty/libs/macos/libSDL3.dylib)
     else()
         message(FATAL_ERROR "HAVE_INTERNAL_SDL set incorrectly; file a bug")
     endif()
 else()
-    find_package(SDL2 REQUIRED)
+    find_package(SDL3 REQUIRED)
 endif()
 
-list(APPEND CLIENT_LIBRARIES ${SDL2_LIBRARIES})
-list(APPEND CLIENT_INCLUDE_DIRS ${SDL2_INCLUDE_DIRS})
-list(APPEND CLIENT_COMPILE_OPTIONS ${SDL2_CFLAGS_OTHER})
-list(APPEND RENDERER_LIBRARIES ${SDL2_LIBRARIES})
-list(APPEND RENDERER_INCLUDE_DIRS ${SDL2_INCLUDE_DIRS})
-list(APPEND RENDERER_COMPILE_OPTIONS ${SDL2_CFLAGS_OTHER})
+list(APPEND CLIENT_LIBRARIES ${SDL3_LIBRARIES})
+list(APPEND CLIENT_INCLUDE_DIRS ${SDL3_INCLUDE_DIRS})
+list(APPEND CLIENT_COMPILE_OPTIONS ${SDL3_CFLAGS_OTHER})
+list(APPEND RENDERER_LIBRARIES ${SDL3_LIBRARIES})
+list(APPEND RENDERER_INCLUDE_DIRS ${SDL3_INCLUDE_DIRS})
+list(APPEND RENDERER_COMPILE_OPTIONS ${SDL3_CFLAGS_OTHER})
